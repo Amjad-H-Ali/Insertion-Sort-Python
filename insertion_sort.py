c9ccf056e146ff5188d89844c36030d4f747e3fb
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-13 06:09:38 -0500

    Store the index before the current element

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,8 +1,10 @@
 print("Insertion Sort!")
 
-# Loop over Array starting from index one
+
 
 array = [4, 2, 7, 96, 23, 45, 67, 3, 2, 3, 3, 9, 0, -1, 26, 0]
 
+# Loop over Array starting from index one
 for i in range(1, len(array)):
-	print(array[i])
+	# Store the index before the current element
+	indx_B4 = i - 1
