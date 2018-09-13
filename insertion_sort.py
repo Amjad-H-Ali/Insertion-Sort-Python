c7c07037349c139fce21eeb513c20ba0851351ef
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-13 06:06:18 -0500

    Iterates over array starting from index 1

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,1 +1,8 @@
 print("Insertion Sort!")
+
+# Loop over Array starting from index one
+
+array = [4, 2, 7, 96, 23, 45, 67, 3, 2, 3, 3, 9, 0, -1, 26, 0]
+
+for i in range(1, len(array)):
+	print(array[i])
