b18aa334140eda0abcba864b3200d2d2f9c610ec
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-13 06:11:46 -0500

    Stores current element in variable

--- insertion_sort.py
+++ insertion_sort.py
@@ -6,5 +6,9 @@ array = [4, 2, 7, 96, 23, 45, 67, 3, 2, 3, 3, 9, 0, -1, 26, 0]
 
 # Loop over Array starting from index one
 for i in range(1, len(array)):
-	# Store the index before the current element
+	
+	# Current element
+	current_elem = array[i]
+
+	# Index before the current element
 	indx_B4 = i - 1
